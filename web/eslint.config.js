921d1bfc11d7f0c436dd01c30486eb9c195f00d6
sfgawrgarf <93918570+sfgawrgarf@users.noreply.github.com>
2026-09-18 22:10:20 +0800

    ci: strengthen quality gates

---
+++ web/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['src/**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    files: ['src/**/*.{ts,vue}'],
+    rules: {
+      'no-undef': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+)
